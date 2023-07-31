48eee2b890a357cd9c2e83b3759663e4e6413887
Evelio Excellenta <velioexcel@gmail.com>
2023-07-31 18:59:12 +0700

    Add

--- 04/exercise4.js
+++ 04/exercise4.js
@@ -24,5 +24,38 @@ function fizzBuzz (n){
     }
     console.log (result)
 }
-
 fizzBuzz(15)
+
+function BMI(weight, height){
+    let bmi = weight/height
+    if (bmi < 18.5){
+        return "less weight"
+    }else if(bmi>=18.5 && bmi<=24.9){
+        return "ideal"
+    }else if(bmi>=25.0 && bmi<=29.9){
+        return "overweight"
+    }else if(bmi>=30.0 && bmi<=39.9){
+        return "very overweight"
+    }else if(bmi>39.9){
+        return "obesity"
+    }
+
+}
+console.log(BMI(65,170))
+
+
+function removeOdd(array){
+    let newArray
+    for (let i=0; i<array.length;i++){  
+        newArray.push(i)
+    }
+}
+const array = [1,2,3,4,5,6,7,8,9]
+console.log()
+
+
+function splitString(string){
+        return string.split(" ");
+    }
+
+console.log(splitString("hello world"))
